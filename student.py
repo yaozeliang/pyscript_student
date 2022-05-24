8580db1224ca6c1c6ba4d30eb2634a759bfba550
yaozeliang <zeliang.yao@efrei.net>
2022-05-24 19:57:46 +0200

    use yaml as db

--- student.py
+++ student.py
@@ -1,12 +1,27 @@
 from dataclasses import dataclass,field
-from typing import AnyStr
+from importlib import import_module
+from typing import AnyStr,Any
+import yaml
+
+DATA_BASE = 'db.yaml'
 
 @dataclass
 class Handler:
-    name: AnyStr =''
+    db = DATA_BASE
     students: list[str] = field(default_factory=list)
+    def __post_init__(self):
+        with open(self.db) as file:
+            self.students = yaml.safe_load(file)['students']['name']
+    
+    def getTotalNumber(self):
+        return len(self.students)
 
-
+    def getAllStudents(self):
+        for e in self.students:
+            node = document.createElement("li")
+            node.setAttribute("class", "list-group-item")
+            node.appendChild(document.createTextNode(e))
+            document.getElementById("studentList").appendChild(node)
 
 def createStudent(*args,**kargs) -> None:
     node = document.createElement("li")
@@ -16,3 +31,7 @@ def createStudent(*args,**kargs) -> None:
     document.getElementById("studentList").appendChild(node)
 
 
+
+h = Handler()
+document.getElementById("total").textContent= f"Total {h.getTotalNumber()} students"
+h.getAllStudents()
